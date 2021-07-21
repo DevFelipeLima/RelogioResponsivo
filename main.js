816559be3bc5a3152bed55882803d4eb695df21d
DevFelipeLima <86861086+DevFelipeLima@users.noreply.github.com>
2021-07-21 13:01:17 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+const hora = document.getElementById('rel-horas')
+const min = document.getElementById('rel-minutos')
+const seg = document.getElementById('rel-segundos')
+
+const relogio = () =>{
+    let data = new Date()
+
+    let hh = data.getHours() * 30
+    let mm = data.getMinutes () * 6
+    let ss = data.getSeconds() * 6
+
+
+    hora.style.transform = `rotateZ(${hh + mm / 12}deg)`
+    min.style.transform = `rotateZ(${mm}deg)`
+    seg.style.transform = `rotateZ(${ss}deg)`
+}
+setInterval(relogio, 1000)
+
+const horadig = document.getElementById('hora'),
+      minutosdig = document.getElementById('minutos'),
+      datadia = document.getElementById('dia'),
+      datames = document.getElementById('mes'),
+      dataano = document.getElementById('ano')
+
+
+const relogiodig= () =>{
+    let datadig = new Date()
+
+    let hh = datadig.getHours ()
+    let mm = datadig.getMinutes()
+    let dia = datadig.getDay()
+    let mes = datadig.getMonth()
+    let ano = datadig.getFullYear()
+
+    if(mm < 10){
+        horadig.innerHTML= `${hh} :`
+        minutosdig.innerHTML= `0${mm}`
+    }else{
+        horadig.innerHTML= `${hh} :`
+        minutosdig.innerHTML= `${mm}`
+    }
+
+   
+
+    let meses = ['Jan', 'Fev', 'Mar', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+
+    datadia.innerHTML=`${dia}`
+    datames.innerHTML=`${meses[mes]}`
+    dataano.innerHTML=`${ano}`
+
+}
+setInterval(relogiodig, 1000)
+
+
